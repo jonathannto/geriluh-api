05de7d362e110b593517a4207158a2d4a0ce90b6
Jonathan Nascimento <nascimento.jonathan@live.com>
2024-06-04 09:02:14 -0300

    Commit inicial.

---
+++ src/test/java/br/eng/jonathan/geriluh_api/GeriluhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eng.jonathan.geriluh_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeriluhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
